0e1f2739e6fc43665af3e623ed512e04944e9ae8
jd <juandiegogarita@gmail.com>
2023-07-04 12:19:01 -0600

    includes pokemon type helper matcher - wip

---
+++ shared/src/commonMain/kotlin/com.jd.pkdx/presentation/Type.kt
@@ -0,0 +1,48 @@
+package com.jd.pkdx.presentation
+
+import androidx.compose.ui.graphics.Color
+import com.jd.pkdx.presentation.theme.PokemonColors
+
+enum class Type {
+    Normal,
+    Fire,
+    Fighting,
+    Water,
+    Flying,
+    Grass,
+    Poison,
+    Electric,
+    Ground,
+    Psychic,
+    Rock,
+    Ice,
+    Bug,
+    Dragon,
+    Ghost,
+    Dark,
+    Steel,
+    Fairy,
+}
+
+fun mapTypeToColor(type: String): Color {
+    return when (Type.valueOf(type)) {
+        Type.Grass -> PokemonColors.Grass
+        Type.Bug -> PokemonColors.Bug
+        Type.Fairy -> PokemonColors.Fairy
+        Type.Fire -> PokemonColors.Fire
+        Type.Flying -> PokemonColors.Flying
+        Type.Water -> PokemonColors.Water
+        Type.Ice -> PokemonColors.Ice
+        Type.Dragon -> PokemonColors.Dragon
+        Type.Normal -> PokemonColors.Normal
+        Type.Fighting -> PokemonColors.Fighting
+        Type.Electric -> PokemonColors.Electric
+        Type.Psychic -> PokemonColors.Psychic
+        Type.Poison -> PokemonColors.Poison
+        Type.Ghost -> PokemonColors.Ghost
+        Type.Ground -> PokemonColors.Ground
+        Type.Rock -> PokemonColors.Rock
+        Type.Dark -> PokemonColors.Dark
+        else -> return Color.Magenta
+    }
+}
