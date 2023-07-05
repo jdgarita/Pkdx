79e6f9a6c1b4d1b15c0561b771d738bb531badbe
jd <juandiegogarita@gmail.com>
2023-07-04 21:53:24 -0600

    ui improvements

--- shared/src/commonMain/kotlin/com.jd.pkdx/presentation/elements/PokemonBadges.kt
+++ shared/src/commonMain/kotlin/com.jd.pkdx/presentation/elements/PokemonBadges.kt
@@ -1,13 +1,19 @@
 package com.jd.pkdx.presentation.elements
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -16,18 +22,29 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.jd.pkdx.presentation.theme.PokemonTypesTheme
 
+enum class PokemonBadgesStyle {
+    VERTICAL, HORIZONTAL
+}
+
 @Composable
 fun PokemonBadges(
+    modifier: Modifier = Modifier,
     pokemonTypes: List<String>,
     colored: Boolean = false,
+    style: PokemonBadgesStyle,
 ) {
+    val numberOfColumns = if (style == PokemonBadgesStyle.VERTICAL) 1 else 2
+    val width = if (style == PokemonBadgesStyle.VERTICAL) 100.dp else 200.dp
     PokemonTypesTheme(types = pokemonTypes) {
-        Column(
-            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+        LazyVerticalGrid(
+            modifier = modifier.width(width),
+            verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically),
+            horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterHorizontally),
+            columns = GridCells.Fixed(numberOfColumns)
         ) {
-            pokemonTypes.forEach { pokemonType ->
+            items(items = pokemonTypes) { pokemonType ->
                 PokemonBadge(pokemonType = pokemonType, colored = colored)
-                Spacer(modifier = Modifier.size(8.dp))
+                Spacer(modifier = Modifier.size(10.dp))
             }
         }
     }
--- shared/src/commonMain/kotlin/com.jd.pkdx/presentation/screens/PokemonDetails.kt
+++ shared/src/commonMain/kotlin/com.jd.pkdx/presentation/screens/PokemonDetails.kt
@@ -2,17 +2,24 @@ package com.jd.pkdx.presentation.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
@@ -22,16 +29,23 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.jd.pkdx.CrossFadeTransitionSpec
 import com.jd.pkdx.DetailsScreen
 import com.jd.pkdx.FadeInTransitionSpec
 import com.jd.pkdx.FadeOutTransitionSpec
 import com.jd.pkdx.domain.Pokemon
 import com.jd.pkdx.presentation.elements.PokemonBadges
+import com.jd.pkdx.presentation.elements.PokemonBadgesStyle
 import com.jd.pkdx.presentation.elements.PokemonImage
 import com.jd.pkdx.presentation.elements.PokemonName
+import com.jd.pkdx.presentation.elements.PokemonNumber
+import com.jd.pkdx.presentation.icons.Female
+import com.jd.pkdx.presentation.icons.Male
 import com.jd.pkdx.presentation.shared.SharedElement
 import com.jd.pkdx.presentation.shared.SharedMaterialContainer
 import com.jd.pkdx.presentation.theme.PokemonTypesTheme
@@ -91,13 +105,25 @@ fun PokemonDetailsScreen(
                 }
             }
 
-            Box {
-                SharedElement(
-                    key = pokemon.name,
-                    screenKey = DetailsScreen,
-                    transitionSpec = CrossFadeTransitionSpec,
-                ) {
-                    PokemonName(pokemonName = pokemon.name)
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                    SharedElement(
+                        key = pokemon.name,
+                        screenKey = DetailsScreen,
+                        transitionSpec = CrossFadeTransitionSpec,
+                    ) {
+                        PokemonName(pokemonName = pokemon.name)
+                    }
+                    SharedElement(
+                        key = "${pokemon.id}${pokemon.name}",
+                        screenKey = DetailsScreen,
+                        transitionSpec = CrossFadeTransitionSpec,
+                    ) {
+                        PokemonNumber(pokemon.id)
+                    }
                 }
 
                 SharedElement(
@@ -105,7 +131,83 @@ fun PokemonDetailsScreen(
                     screenKey = DetailsScreen,
                     transitionSpec = CrossFadeTransitionSpec
                 ) {
-                    PokemonBadges(pokemonTypes = pokemon.typeOfPokemon, colored = true)
+                    PokemonBadges(
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        pokemonTypes = pokemon.typeOfPokemon,
+                        colored = true,
+                        style = PokemonBadgesStyle.HORIZONTAL
+                    )
+                }
+
+                Text(
+                    text = pokemon.description,
+                    textAlign = TextAlign.Justify,
+                    lineHeight = 24.sp
+                )
+                Surface(
+                    shape = RoundedCornerShape(12.dp),
+                ) {
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 16.dp, horizontal = 20.dp)
+                            .align(Alignment.CenterHorizontally)
+                    ) {
+                        Column(Modifier.weight(1f)) {
+                            Label(text = "Height")
+                            Spacer(Modifier.height(12.dp))
+                            Text("${pokemon.height}m")
+                        }
+                        Column(Modifier.weight(1f)) {
+                            Label(text = "Weight")
+                            Spacer(Modifier.height(12.dp))
+                            Text("${pokemon.weight}kg")
+                        }
+                    }
+                }
+                Column {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Label(
+                            text = "Gender",
+                            modifier = Modifier
+                                .weight(1f)
+                                .padding(end = 12.dp)
+                        )
+                        if (pokemon.genderRate != -1) {
+                            Row(Modifier.weight(2f)) {
+                                val femaleGenderRate = pokemon.genderRate * 12.5
+                                Row {
+                                    Icon(
+                                        modifier = Modifier.requiredSize(26.dp),
+                                        imageVector = Male,
+                                        contentDescription = null,
+                                        tint = Color(0xff6C79DB)
+                                    )
+                                    Spacer(Modifier.width(2.dp))
+                                    Text("${100 - femaleGenderRate}%")
+                                }
+                                Spacer(Modifier.width(12.dp))
+                                Row {
+                                    Icon(
+                                        modifier = Modifier.requiredSize(26.dp),
+                                        imageVector = Female,
+                                        contentDescription = null,
+                                        tint = Color(0xffF0729F)
+                                    )
+                                    Spacer(Modifier.width(2.dp))
+                                    Text("$femaleGenderRate%")
+                                }
+                            }
+                        } else {
+                            Text(
+                                "Genderless",
+                                modifier = Modifier.weight(2.2f)
+                            )
+                        }
+                    }
                 }
             }
         }
@@ -121,4 +223,24 @@ fun PokemonDetailsScreen(
             )
         }
     }
+}
+
+@Composable
+fun Label(
+    modifier: Modifier = Modifier,
+    text: String,
+    emphasis: Emphasis = Emphasis.Medium,
+) {
+    Text(
+        text = text,
+        modifier = modifier.graphicsLayer {
+            alpha = emphasis.alpha
+        },
+    )
+}
+
+enum class Emphasis(val alpha: Float) {
+    Disabled(0.5f),
+    Medium(0.7f),
+    High(1f)
 }
